f33fb45883b3c1fd6ebeb0dcd6b06e030ba6e600
Kássio Silva <Kassio.Sa@ibm.com>
2023-08-22 15:15:46 -0300

    refactor: ajuste no adapter da listagem

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -5,17 +5,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.todolist.adapter.TasksAdapter
 import com.example.todolist.databinding.ActivityMainBinding
+import com.example.todolist.databinding.TaskBinding
+import com.example.todolist.model.Task
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
-    private var adapter = TasksAdapter()
     private lateinit var binding: ActivityMainBinding
-    private val items = listOf<String>("TESTE")
+    private val tasks = mutableListOf<Task>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,15 +21,13 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        supportActionBar?.hide()
-
         handleList()
-
         listeners()
     }
 
     override fun onClick(view: View) {
         if (view.id == R.id.image_button_add) {
+            // handleAddTask()
         }
     }
 
@@ -40,15 +36,38 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     private fun handleList() {
-        binding.recyclerTasks.layoutManager =
-            LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        binding.recyclerTasks.layoutManager = LinearLayoutManager(this)
+        binding.recyclerTasks.adapter = TasksAdapter(tasks)
 
-        binding.recyclerTasks.adapter = adapter
 
-        adapter.updatedTasks(items)
+        // Lista vazia
+        // adapter.updatedTasks(items)
+        // val emptyObserver = EmptyObserver(binding.recyclerTasks, binding.emptyContainer.root)
+        // adapter.registerAdapterDataObserver(emptyObserver)
+    }
 
-        val emptyObserver = EmptyObserver(binding.recyclerTasks, binding.emptyContainer.root)
+    private inner class TasksAdapter(
+        private val tasks: List<Task>
+    ) : RecyclerView.Adapter<TasksAdapter.TaskViewHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
+            val view = TaskBinding.inflate(LayoutInflater.from(parent.context), parent, false)
 
-        adapter.registerAdapterDataObserver(emptyObserver)
+            return TaskViewHolder(view)
+        }
+
+        override fun getItemCount(): Int {
+            return 60
+        }
+
+        override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
+            //val task = tasks[position]
+            //holder.bind(task)
+        }
+
+        inner class TaskViewHolder(private val bind: TaskBinding) : RecyclerView.ViewHolder(bind.root) {
+            fun bind(task: Task) {
+                bind.checkboxTask.text = task.description
+            }
+        }
     }
 }
